c7e93e00a1488642fd4542ce163d967062d00b0e
Joshua Bontor <bontorjoshua@gmail.com>
2025-02-18 12:50:47 +0800

    instructor_login changes

--- src/components/instructor_login/instructor_login.jsx
+++ src/components/instructor_login/instructor_login.jsx
@@ -13,7 +13,7 @@ const InstructorLogin = () => {
   
     try {
       // Check if the email exists in the instructors collection
-      const response = await axios.get("https://aissistant-omega.vercel.app/api/checkInstructorEmail", {
+      const response = await axios.get("https://aissistant-three.vercel.app/api/checkInstructorEmail", {
         params: { email: decodedToken.email },
       });
   
