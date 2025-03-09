f64a8ed4878110046f8cf17926ff7d1952fc9f59
Joshua Bontor <bontorjoshua@gmail.com>
2025-03-09 16:46:47 +0800

    landing

--- frontend/src/components/aissistant/aissistant.jsx
+++ frontend/src/components/aissistant/aissistant.jsx
@@ -28,15 +28,32 @@ const AIssistant = () => {
           <div className='landing-aissistant'><h1 className='ai'>AI</h1><h1 className='landing-welcome'>ssistant</h1></div>
         </div>
         <div className="aissistant">
-          <div className='aissistant-text'>
-            <h1>Enhance Your Programming Skills with AIssistant</h1>
-            <p className='single-text'>Master coding with AI-powered guidance and real-time feedback.</p>
-            <p className='single-text'>Learn, practice, and improve—whether you're a beginner or an advanced developer.</p>
-            <span className="combined-text">Master coding with AI-powered guidance and real-time feedback. Learn, practice, and improve—whether you're a beginner or an advanced developer.</span>
-            <button onClick={() => handleBoxClick()} className='aissistant-user-type'>Login</button>
-          </div>
+            <div className="aissistant-info">
+              <span>
+              <h2>Student</h2>
+              <p>1. Login using nemsu workspace account</p>
+              <p>2. Interact with AIssistant chat</p>
+              <p>3. Enter class code</p>
+              <p>4. Read and study learning materials</p>
+              <p>5. Test your understanding with quizzes</p>
+              </span>
+
+              <span>
+                <h2>Instructor</h2>
+                <p>1. Loging using nemsu workspace account</p>
+                <p>2. Create learning materials</p>
+                <p>3. View students' frequently asked questions</p>
+              </span>
+            </div>
           <div className='aissistant-logo'>
             <img src={logo} alt="AIssistant-logo" className='aissistant-icon' />
+            <div className='aissistant-text'>
+              <h1>Enhance Your Programming Skills with AIssistant</h1>
+              <p className='single-text'>Master coding with AI-powered guidance and real-time feedback.</p>
+              <p className='single-text'>Learn, practice, and improve—whether you're a beginner or an advanced developer.</p>
+              <span className="combined-text">Master coding with AI-powered guidance and real-time feedback. Learn, practice, and improve—whether you're a beginner or an advanced developer.</span>
+              <button onClick={() => handleBoxClick()} className='aissistant-user-type'>Login</button>
+            </div>
           </div>
         </div>
       </div>
