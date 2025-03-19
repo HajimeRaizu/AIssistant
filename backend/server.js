0e37cda0e47f0f906e9b08d0381dc7c4b4171120
Joshua Bontor <bontorjoshua@gmail.com>
2025-03-19 12:34:40 +0800

    endpoint

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ const qwen = new OpenAI({
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://aissistant-pi.vercel.app',
+  origin: "*",
   methods: "GET, POST, PUT, DELETE, OPTIONS",
   credentials: true,
   allowedHeaders: "Content-Type, Authorization"
